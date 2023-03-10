2a2c3b44ef671b1ce68e8bb40a892c8a12db703d
mohammedwaseem0udy <mohammedwaseem0udy@gmail.com>
2023-03-10 23:01:07 +0530

    Added EJS view engine and logout functionality

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ let bodyParser = require("body-parser");
 let _ = require("lodash");
 let users = require("./users");
 let notification = require("./notification");
+let ejs = require('ejs');
 const app = express();
 
+
+// middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
+// paths
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/login.html");
+    res.render('login');
 });
 
 app.get("/signup", function (req, res) {
-    res.sendFile(__dirname + "/signup.html");
+    res.render('signup');
 });
 
 app.post("/create-user", async function (req, res) {
@@ -49,12 +53,18 @@ app.post('/validate-login-user', async function(req, res) {
     if(response[1]) {
         if(response[0] === 'success') {
             notification.sendNotification('Login Successful', response[1]);
+            res.render('success', {name: response[1]});
         } else {
             notification.sendNotification('Login Failed', response[1]);
+            res.render('failure', {message: response[1]})
         }
-        res.send(`<h1>${response[1]}</h1>`);
+        
     } else {
         notification.sendNotification('Login Failed', response[1]);
-        res.send(`<h1>Something went wrong</h1>`);
+        res.render('failure', {message: response[1]});
     }
+})
+
+app.post('/logout', function(req, res) {
+    res.render('login');
 })
